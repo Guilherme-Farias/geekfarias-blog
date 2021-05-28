80a4153e73309d284d4257667435c065b339d01d
Guilherme Farias <guicfarias11@gmail.com>
2021-05-27 22:09:01 -0300

    Estilização global finalizada

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,14 @@
+import Head from 'next/head';
+
 export const Home: React.FC = () => {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <Head>
+        <title>Posts | GeekFarias.blog</title>
+      </Head>
+      <h1>Hello World</h1>
+    </>
+  );
 };
 
 export default Home;
